b982e6c4a73fbdd746da77670349c710e4eeff4c
Basaranovich Andrei <basaranovich2505@gmail.com>
2023-12-08 13:56:00 +0300

    Revert "namesaysall"
    This reverts commit 4734a3f3689d770646283f4c7b23c5b4c7f3da05.

--- h2-database-springboot-jpa/h2-db-memory/src/main/java/com/example/h2dbmemory/repository/PersonRep.java
+++
@@ -1,9 +0,0 @@
-package com.example.h2dbmemory.repository;
-
-import com.example.h2dbmemory.db.Person;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRep extends CrudRepository<Person, Long> {
-}
